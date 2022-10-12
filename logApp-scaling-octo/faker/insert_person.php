dd615d9a6a1fe6dfe40d04c0e4093f939a8bd02b
annacuadra <202080161@psu.palawan.edu.ph>
2022-10-13 03:31:06 +0800

    changes

---
+++ logApp-scaling-octo/faker/insert_person.php
@@ -0,0 +1,17 @@
+<?php
+
+require('vendor/autoload.php');
+$faker = Faker\Factory::create();
+$conn = mysqli_connect("sql6.freesqldatabase.com","sql6525708","1JupD32AEN","sql6525708");
+
+if(!$conn)
+{   
+die(mysqli_error());
+}  
+
+for ($i=0; $i <= 10; $i++){   
+$sql = "INSERT INTO sql6525708.person(lastname, firstname, address, logdt) values('".$faker->lastName."','".$faker->firstName."', '".$faker->address."', '".$faker->date($format = 'Y-m-d H:i:s', $max = 'now')."')";     
+
+mysqli_query($conn, $sql);}
+
+?>
