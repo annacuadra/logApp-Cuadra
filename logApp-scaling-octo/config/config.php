37ff2876086594c837f298de2c537f647c322fb6
annacuadra <202080161@psu.palawan.edu.ph>
2022-10-12 15:38:01 +0800

    logApp

---
+++ logApp-scaling-octo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525708');
+	define('DB_PASS', '1JupD32AEN');
+	define('DB_NAME', 'sql6525708');
